1756fe60ad83577caed5a81775183df53f5c83f7
harshal <harshalmahapure44@gmail.com>
2023-04-21 15:07:25 +0530

    create seperate route for signin signup and a check in auth view if user is authenticated or not

--- src/authentication/urls.py
+++ src/authentication/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.auth,name="auth_user"),
+    path('signup', views.signup,name="signup"),
     path('signin', views.login,name="login"),
+    path('signupuser', views.signupUser,name="signupUser"),
 ]
--- src/authentication/views.py
+++ src/authentication/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render,redirect
-
+from django.contrib.auth.models import User
 # Create your views here.
+
 def auth(request):
-    return render(request,"authentication/signup.html")
+    if request.user.is_authenticated:
+        return render(request,"music_player/home.html")
+    return redirect("/signup")
+
+def signup(reqeust):
+    return render(reqeust,"authentication/signup.html")
+
+
 
 def login(request):
     return render(request,"authentication/signin.html")
+
+
+def signupUser(reqeust):
+    pass
